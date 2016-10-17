e2a1b4da23d49fe460dbe454315de946e703493a
Salvador Martinez-Bartolome <salvador@scripps.edu>
2016-10-17 14:18:55 -0700

    Initial push of dbIndex

---
+++ src/main/java/edu/scripps/yates/dbindex/util/PeptideNotFoundInDBIndexException.java
@@ -0,0 +1,14 @@
+package edu.scripps.yates.dbindex.util;
+
+public class PeptideNotFoundInDBIndexException extends RuntimeException {
+
+	public PeptideNotFoundInDBIndexException(String message) {
+		super(message);
+	}
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -3865382579925568748L;
+
+}
