861b147019788d5d5aab4aea7c22d3fcfdfef4b7
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-05-04 15:35:40 -0700

    making it extensible

--- src/main/java/edu/scripps/yates/dbindex/DynByteBuffer.java
+++ src/main/java/edu/scripps/yates/dbindex/DynByteBuffer.java
@@ -19,19 +19,25 @@ public class DynByteBuffer {
 	private static Logger log = Logger.getLogger(DynByteBuffer.class);
 	private static int maxCapacity = 0;
 
-	DynByteBuffer() {
+	public DynByteBuffer() {
 		data = new byte[curCapacity];
 	}
 
-	void add(byte[] toAdd) {
+	public void add(byte toAdd) {
+		final byte[] array = new byte[1];
+		array[0] = toAdd;
+		add(array);
+	}
+
+	public void add(byte[] toAdd) {
 		final int toAddLen = toAdd.length;
 		if (curCapacity < curSize + toAddLen) {
-			int newCapacity = (curCapacity + toAddLen) * 3;
+			final int newCapacity = (curCapacity + toAddLen) * 3;
 			if (newCapacity > maxCapacity) {
 				log.debug("newCapacity= " + newCapacity);
 				maxCapacity = newCapacity;
 			}
-			byte[] newData = new byte[newCapacity];
+			final byte[] newData = new byte[newCapacity];
 			System.arraycopy(data, 0, newData, 0, curSize);
 			curCapacity = newCapacity;
 			data = newData;
@@ -42,34 +48,44 @@ public class DynByteBuffer {
 		curSize += toAddLen;
 	}
 
-	void clear() {
+	public void clear() {
 		curSize = 0;
 		curCapacity = DEFAULT_CAPACITY * 4;
 		data = new byte[curCapacity];
 
 	}
 
-	int getSize() {
+	public int getSize() {
 		return curSize;
 	}
 
-	byte[] getData() {
-		if (data.length % 4 != 0)
-			System.out.println("MALO");
+	public byte[] getData() {
 		final byte[] copyOfRange = Arrays.copyOfRange(data, 0, curSize);
-		if (copyOfRange.length % 4 != 0)
-			System.out.println("MALO");
 		return copyOfRange;
 	}
 
 	public static byte[] toByteArray(int myInteger) {
-		return ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN)
-				.putInt(myInteger).array();
+		return ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(myInteger).array();
 	}
 
-	public static int toInt(byte[] byteBarray) {
-		return ByteBuffer.wrap(byteBarray).order(ByteOrder.LITTLE_ENDIAN)
-				.getInt();
+	public static int toInt(byte[] byteArray) {
+		return ByteBuffer.wrap(byteArray).order(ByteOrder.LITTLE_ENDIAN).getInt();
+	}
+
+	public static byte[] toByteArray(char myChar) {
+		return ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putChar(myChar).array();
+	}
+
+	public static char toChar(byte[] byteArray) {
+		return ByteBuffer.wrap(byteArray).order(ByteOrder.LITTLE_ENDIAN).getChar();
+	}
+
+	public static byte[] toByteArray(short myShort) {
+		return ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putShort(myShort).array();
+	}
+
+	public static short toShort(byte[] byteBarray) {
+		return ByteBuffer.wrap(byteBarray).order(ByteOrder.LITTLE_ENDIAN).getShort();
 	}
 
 	// public static byte[] toByteArray(float theFloat) {
@@ -78,8 +94,7 @@ public class DynByteBuffer {
 	// }
 
 	public static byte[] toByteArray(double theDouble) {
-		return ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN)
-				.putDouble(theDouble).array();
+		return ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putDouble(theDouble).array();
 	}
 
 	// public static float toFloat(byte[] byteBarray) {
@@ -88,7 +103,10 @@ public class DynByteBuffer {
 	// }
 
 	public static double toDouble(byte[] byteBarray) {
-		return ByteBuffer.wrap(byteBarray).order(ByteOrder.LITTLE_ENDIAN)
-				.getDouble();
+		return ByteBuffer.wrap(byteBarray).order(ByteOrder.LITTLE_ENDIAN).getDouble();
+	}
+
+	public static int toInt(byte myByte) {
+		return myByte & (0xff);
 	}
 }
